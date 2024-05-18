e38923d6aaf5b5a3ba889ae1d1fbb2de2ba53888
kirittosemi <sunny7104kevin@gmail.com>
2024-05-18 20:57:42 +0900

    test

--- Assets/Shop/SelectExitShop.cs
+++ Assets/Shop/SelectExitShop.cs
@@ -7,7 +7,7 @@ public class SelectExitShop : MonoBehaviour
 {
     public void Exit()
     {
-
-        SceneManager.LoadScene("SampleScene");
+        PlayerPrefs.SetInt("GoldPerSec", DataController.GetInstance().GetGoldPerSec());
+        SceneManager.LoadScene("Menu");
     }
 }
